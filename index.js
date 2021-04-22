e56de85ef8cf793cc122b1a6648f53f4e54baa39
dcarl661 <dcarl@earthlink.net>
2021-04-22 14:58:18 -0700

    initial checkin

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const socket = require("socket.io");
+const app = express();
+
+//starts the server
+
+let server = app.listen(4000, function (){
+    console.log("service is started");
+})
+
+app.use(express.static("public"));
